8237759dc670c8a630c29eae194c30786eb26417
Kelvin Wu <wuhfhf@gmail.com>
2022-02-19 21:00:35 +0800

    Switch to desktop

---
+++ JLox/src/com/craftinginterpreters/tool/GenerateAst.java
@@ -0,0 +1,24 @@
+package com.craftinginterpreters.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if(args.length != 1){
+            System.err.println("Usage: generate_asdt <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAst(outputDir,"Expr", Arrays.asList(
+            "Binary     : Expr left, Token operator , Expr right",
+            "Grouping   : Expr expression",
+            "Literal    : Object value",
+            "Unary     : Token operator, Expr right"
+        ));
+
+        private static void defineAst(String outputDir,String baseName, String)
+    }    
+}
